fa5f059de25cff7a2cab8ea0019e1e890af9669d
Jonathan Shambaugh <jshambaugh96@gmail.com>
2022-12-12 10:27:17 -0600

    commit

--- import_tool_project/src/main.rs
+++ import_tool_project/src/main.rs
@@ -1,3 +1,16 @@
+use std::time::Instant;
+
+
+fn done(error: bool) {
+    if error {
+        std::process::exit(-1);
+    } else {
+        std::process::exit(0);
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let test = Instant::now();
+
 }
--- import_tool_project/target/debug/deps/import_tool_project-1bee8a103bc300bc.d
+++ import_tool_project/target/debug/deps/import_tool_project-1bee8a103bc300bc.d
@@ -1,5 +1,5 @@
-c:\Users\jsham\Documents\code\EDC-Import-Tool-V2\import_tool_project\target\debug\deps\import_tool_project-1bee8a103bc300bc.rmeta: src\main.rs
+c:\Users\jsham\Documents\code\EDC-Import-Tool-V2_dev\import_tool_project\target\debug\deps\import_tool_project-1bee8a103bc300bc.rmeta: src\main.rs
 
-c:\Users\jsham\Documents\code\EDC-Import-Tool-V2\import_tool_project\target\debug\deps\import_tool_project-1bee8a103bc300bc.d: src\main.rs
+c:\Users\jsham\Documents\code\EDC-Import-Tool-V2_dev\import_tool_project\target\debug\deps\import_tool_project-1bee8a103bc300bc.d: src\main.rs
 
 src\main.rs:
