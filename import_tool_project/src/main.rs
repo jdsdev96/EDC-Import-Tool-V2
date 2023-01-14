34855916aa48bda577a6581f80f8ba0ef80bf310
Jonathan Shambaugh <jshambaugh96@gmail.com>
2023-01-14 10:10:34 -0600

    g

--- import_tool_project/src/main.rs
+++ import_tool_project/src/main.rs
@@ -1,5 +1,6 @@
 use std::time::Instant;
-
+use std::fs;
+use std::path::Path;
 
 
 fn done(error: bool) {
@@ -13,6 +14,17 @@ fn done(error: bool) {
 
 fn main() {
     let start = Instant::now();
+    
+    let mut debug_file_exist = false;
+
+    //gets all file names in working dir
+    let paths = fs::read_dir("./").unwrap();
+
+    //loops for dispplay
+    for path in paths {
+        println!("{}", path.unwrap().path().display());
+    }
+
     let duration = start.elapsed();
     println!("Execution time: {:?}", duration);
     done(false);
